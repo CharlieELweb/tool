b29c002adc4a7efff52241e9637d3b64be5f32cd
CharlieEL <charlieelemail@gmail.com>
2025-03-01 12:53:04 +0800

    Add files via upload

--- LaTeXGenerator/script.js
+++ LaTeXGenerator/script.js
@@ -7,9 +7,16 @@ var integralCount = 0;
 var sScriptsCount = 0;
 var redicalCount = 0;
 var productCount = 0;
+var bracketsSCount = 0;
+var bracketsMCount = 0;
+var bracketsLCount = 0;
+var hatCount = 0;
+var barCount = 0;
+var shrink = false;
+
 const equation = document.getElementById('equation');
 const source = document.getElementById('source');
-const supportedClasses = ["textBox", "frac", "sscr", "sum", "limit", "integral", "redical", "product"];
+const supportedClasses = ["textBox", "frac", "sscr", "sum", "limit", "integral", "redical", "product", "bracketsS", "bracketsM", "bracketsL", "hat", "bar"];
 
 AddText();
 source.addEventListener('change', Render);
@@ -25,6 +32,11 @@ function Clear() {
     sScriptsCount = 0;
     redicalCount = 0;
     productCount = 0;
+    bracketsSCount = 0;
+    bracketsMCount = 0;
+    bracketsLCount = 0;
+    hatCount = 0;
+    barCount = 0;
     while (equation.firstChild) {
         equation.removeChild(equation.lastChild);
     }
@@ -188,10 +200,10 @@ function AddS_Scripts() {
     newElement.id = "sscr" + sScriptsCount;
     newElement.innerHTML = `
         <div class="textBox" id="sscr${sScriptsCount}U">
-            ${AddTextElement(`text${textCount}`, false)}
+            ${AddTextElement(`text${textCount}`, true)}
         </div>
         <div class="textBox" id="sscr${sScriptsCount}D">
-            ${AddTextElement(`text${textCount + 1}`, false)}
+            ${AddTextElement(`text${textCount + 1}`, true)}
         </div>
     `;
     textCount++;
@@ -207,7 +219,13 @@ function Remove() {
     
     var addPosition = focusElement.parentElement;
     if(addPosition != equation) {
-        addPosition.parentElement.parentElement.removeChild(addPosition.parentElement);
+        console.log(addPosition.childElementCount);
+        if(addPosition.childElementCount == 1) {
+            addPosition.parentElement.parentElement.removeChild(addPosition.parentElement);
+        } else {
+            addPosition.removeChild(focusElement);
+        }
+        
     } else {
         addPosition.removeChild(focusElement);
     }
@@ -229,6 +247,105 @@ function AddText() {
     focusElement = newElement;
     AdjustInputSize();
 }
+
+function AddHat() {
+    var addPosition = GetFocusPosition();
+    hatCount++;
+    textCount++;
+    let newElement = document.createElement('div');
+    newElement.className = "hat";
+    newElement.id = "hat" + hatCount;
+    newElement.innerHTML = `
+        <div class="toppers">^</div>
+        <div class="textBox" id="hat${hatCount}Content">
+            ${AddTextElement(`text${textCount}`, false)}
+        </div>
+    `;
+    AppendElement(addPosition, newElement);
+    focusElement = newElement;
+    AddText();
+    AdjustInputSize();
+}
+
+function AddBar() {
+    var addPosition = GetFocusPosition();
+    barCount++;
+    textCount++;
+    let newElement = document.createElement('div');
+    newElement.className = "bar";
+    newElement.id = "bar" + hatCount;
+    newElement.innerHTML = `
+    <div class="toppers">-</div>
+        <div class="textBox" id="bar${hatCount}Content">
+            ${AddTextElement(`text${textCount}`, false)}
+        </div>
+    `;
+    AppendElement(addPosition, newElement);
+    focusElement = newElement;
+    AddText();
+    AdjustInputSize();
+}
+
+function AddBracketsS() {
+    var addPosition = GetFocusPosition();
+    bracketsSCount++;
+    textCount++;
+    let newElement = document.createElement('div');
+    newElement.className = "bracketsS";
+    newElement.id = "bracketsS" + bracketsSCount;
+    newElement.innerHTML = `
+        <div class="brackets">(</div>
+        <div class="textBox" id="bracketsS${bracketsSCount}Content">
+            ${AddTextElement(`text${textCount}`, false)}
+        </div>
+        <div class="brackets">)</div>
+    `;
+    AppendElement(addPosition, newElement);
+    focusElement = newElement;
+    AddText();
+    AdjustInputSize();
+}
+
+function AddBracketsM() {
+    var addPosition = GetFocusPosition();
+    bracketsMCount++;
+    textCount++;
+    let newElement = document.createElement('div');
+    newElement.className = "bracketsM";
+    newElement.id = "bracketsM" + bracketsMCount;
+    newElement.innerHTML = `
+        <div class="brackets">[</div>
+        <div class="textBox" id="bracketsM${bracketsMCount}Content">
+            ${AddTextElement(`text${textCount}`, false)}
+        </div>
+        <div class="brackets">]</div>
+    `;
+    AppendElement(addPosition, newElement);
+    focusElement = newElement;
+    AddText();
+    AdjustInputSize();
+}
+
+function AddBracketsL() {
+    var addPosition = GetFocusPosition();
+    bracketsLCount++;
+    textCount++;
+    let newElement = document.createElement('div');
+    newElement.className = "bracketsL";
+    newElement.id = "bracketsL" + bracketsLCount;
+    newElement.innerHTML = `
+        <div class="brackets">{</div>
+        <div class="textBox" id="bracketsL${bracketsLCount}Content">
+            ${AddTextElement(`text${textCount}`, false)}
+        </div>
+        <div class="brackets">}</div>
+    `;
+    AppendElement(addPosition, newElement);
+    focusElement = newElement;
+    AddText();
+    AdjustInputSize();
+}
+
 function AddFraction() {
     var addPosition = GetFocusPosition();
     fracCount++;
@@ -252,18 +369,146 @@ function AddFraction() {
     AdjustInputSize();
 }
 
+function Shrink() {
+    shrink = !shrink;
+    var shrinkBtn = document.getElementById("btn_shrink");
+    if(shrink) {
+        shrinkBtn.innerHTML = "Expand";
+    } else {
+        shrinkBtn.innerHTML = "Shrink";
+    }
 
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach(ipt => {
+        if(shrink) {
+            ipt.classList.add("shrink");
+        } else {
+            ipt.classList.remove("shrink");
+        }
+    });
+}
 function AdjustInputSize() {
-    
     const inputs = document.querySelectorAll('input');
     inputs.forEach(ipt => {
         
         ipt.addEventListener('input', ResizeInput);
         ipt.addEventListener('click', ChangeFocus);
-        
+        ipt.addEventListener('keydown', function (e) {
+            if(e.key == 'Backspace') {
+                if(ipt.value == '') {
+                    Remove();
+                }
+            }
+            ResizeInput.call(ipt);
+        }, false);
+        ipt.addEventListener('keyup', function (e) {
+            if(ipt.parentElement != focusElement) {
+                return;
+            }
+            if(e.key == '^') {
+                AddS_Scripts();
+                var ElementU = document.getElementById('text' + (textCount - 2) + '_Input');
+                ElementU.focus();
+                SetFocus(ElementU);
+                ipt.value = ipt.value.slice(0, -1);
+            } else if(e.key == '_'){
+                AddS_Scripts();
+                var ElementU = document.getElementById('text' + (textCount - 1) + '_Input');
+                ElementU.focus();
+                SetFocus(ElementU);
+                ipt.value = ipt.value.slice(0, -1);
+            } else if(e.key == '(' || e.key == '[' || e.key == '{') {
+                switch(e.key) {
+                    case '(':
+                        AddBracketsS();
+                        break;
+                    case '[':
+                        AddBracketsM();
+                        break;
+                    case '{':
+                        AddBracketsL();
+                        break;
+                    default:
+                        AddBracketsL();
+                        break;
+                }
+                
+                var Element = document.getElementById('text' + (textCount - 1) + '_Input');
+                ipt.value = ipt.value.slice(0, -1);
+                Element.focus();
+                SetFocus(Element);
+                ResizeInput.call(Element);
+            } else if(e.key == ' ') {
+                if(ipt.value.slice(-2) == '/ ') {
+                    AddFraction();
+                    var ElementU = document.getElementById('text' + (textCount - 2) + '_Input');
+                    var ElementD = document.getElementById('text' + (textCount - 1) + '_Input');
+                    ipt.value = ipt.value.slice(0, -2);
+                    ElementU.value = ipt.value;
+                    ipt.value = "";
+                    if(ElementU.value == '') {
+                        ElementU.focus();
+                        SetFocus(ElementU);
+                    } else {
+                        ElementD.focus();
+                        SetFocus(ElementD);
+                        ResizeInput.call(ElementU);
+                    }
+                } else if (ipt.value.slice(-5) == '\\sum ') {
+                    AddSum();
+                    var ElementD = document.getElementById('text' + (textCount - 1) + '_Input');
+                    ElementD.focus();
+                    SetFocus(ElementD);
+                    ipt.value = ipt.value.slice(0, -5);
+                } else if (ipt.value.slice(-6) == '\\prod ') {
+                    AddProduct();
+                    var ElementD = document.getElementById('text' + (textCount - 1) + '_Input');
+                    ElementD.focus();
+                    SetFocus(ElementD);
+                    ipt.value = ipt.value.slice(0, -6);
+                } else if (ipt.value.slice(-5) == '\\lim ') {
+                    AddLimit();
+                    var ElementD = document.getElementById('text' + (textCount - 1) + '_Input');
+                    ElementD.focus();
+                    SetFocus(ElementD);
+                    ipt.value = ipt.value.slice(0, -6);
+                } else if (ipt.value.slice(-5) == '\\int ') {
+                    AddIntegral();
+                    var ElementD = document.getElementById('text' + (textCount - 1) + '_Input');
+                    ElementD.focus();
+                    SetFocus(ElementD);
+                    ipt.value = ipt.value.slice(0, -6);
+                } else if (ipt.value.slice(-6) == '\\sqrt ') {
+                    AddRedical();
+                    var ElementD = document.getElementById('text' + (textCount - 1) + '_Input');
+                    ElementD.focus();
+                    SetFocus(ElementD);
+                    ipt.value = ipt.value.slice(0, -6);
+                } else if (ipt.value.slice(-5) == '\\bar ') {
+                    AddBar();
+                    var ElementD = document.getElementById('text' + (textCount - 1) + '_Input');
+                    ElementD.focus();
+                    SetFocus(ElementD);
+                    ipt.value = ipt.value.slice(0, -5);
+                } else if (ipt.value.slice(-5) == '\\hat ') {
+                    AddHat();
+                    var ElementD = document.getElementById('text' + (textCount - 1) + '_Input');
+                    ElementD.focus();
+                    SetFocus(ElementD);
+                    ipt.value = ipt.value.slice(0, -5);
+                }
+            }
+            console.log(e.key);
+            ResizeInput.call(ipt);
+        }, false);
         ipt.addEventListener('change', Generate);
+        
         ResizeInput.call(ipt);
     })
+}
+function SetFocus(element) {
+    focusElement = element.parentElement;
+    console.log(focusElement);
 }
 
 function ChangeFocus() {
@@ -362,7 +607,7 @@ function GenerateAllChild(element) {
                     result += "}_{";
                     result += GenerateAllChild(elementD);
                     result += "}";
-                }  else if(child.className == 'redical') {
+                } else if(child.className == 'redical') {
                     var elementU = child.querySelector('#' + child.id + 'U');
                     var elementD = child.querySelector('#' + child.id + 'D');
                     console.log(elementU);
@@ -372,6 +617,29 @@ function GenerateAllChild(element) {
                     result += "]{";
                     result += GenerateAllChild(elementD);
                     result += "}";
+                } else if(child.className == 'bracketsS') {
+                    var element = child.querySelector('#' + child.id + 'Content');
+                    result += "\\left (";
+                    result += GenerateAllChild(element);
+                    result += "\\right )";
+                } else if(child.className == 'bracketsM') {
+                    var element = child.querySelector('#' + child.id + 'Content');
+                    result += "\\left [";
+                    result += GenerateAllChild(element);
+                    result += "\\right ]";
+                } else if(child.className == 'bracketsL') {
+                    var element = child.querySelector('#' + child.id + 'Content');
+                    result += "\\left \\{";
+                    result += GenerateAllChild(element);
+                    result += "\\right \\}";
+                } else if(child.className == 'hat') {
+                    var element = child.querySelector('#' + child.id + 'Content');
+                    result += "\\hat";
+                    result += GenerateAllChild(element);
+                } else if(child.className == 'bar') {
+                    var element = child.querySelector('#' + child.id + 'Content');
+                    result += "\\bar";
+                    result += GenerateAllChild(element);
                 } else {
                     result += GenerateAllChild(child);
                 }
@@ -395,9 +663,39 @@ function Generate() {
 function ReplaceKeywords(input) {
     var output = input;
 
-    output = output.replace(">=", " \\geq ");
-    output = output.replace("<=", " \\leq ");
-    output = output.replace("->", " \\to ");
+    output = output.replace("\\a", "\\alpha ");
+    output = output.replace("\\b", "\\beta ");
+    output = output.replace("\\d", "\\delta ");
+    output = output.replace("\\e", "\\epsilon ");
+    output = output.replace("\\g", "\\gamma ");
+    output = output.replace("\\h", "\\eta ");
+    output = output.replace("\\i", "\\iota ");
+    output = output.replace("\\k", "\\kappa ");
+    output = output.replace("\\l", "\\lambda ");
+    output = output.replace("\\m", "\\mu ");
+    output = output.replace("\\n", "\\nu ");
+    output = output.replace("\\o", "\\omega ");
+    output = output.replace("\\ph", "\\varphi ");
+    output = output.replace("\\p", "\\pi ");
+    output = output.replace("\\r", "\\rho ");
+    output = output.replace("\\s", "\\sigma ");
+    output = output.replace("\\t", "\\tau ");
+    output = output.replace("\\u", "\\upsilon ");
+    output = output.replace("\\x", "\\chi ");
+    output = output.replace("\\z", "\\zeta ");
+
+    output = output.replace(">=", "\\geq ");
+    output = output.replace("<=", "\\leq ");
+    output = output.replace("<->", "\\leftrightarrow ")
+    output = output.replace("-->", "\\longrightarrow ")
+    output = output.replace("->", "\\to ")
+    output = output.replace("<--", "\\longleftarrow ")
+    output = output.replace("<-", "\\gets ")
+    
+    output = output.replace("~", "\\sim ");
+    output = output.replace("*", "\\cdot ");
+    output = output.replace("+-", "\\pm ");
+    output = output.replace("-+", "\\mp ");
     return output;
 }
 
